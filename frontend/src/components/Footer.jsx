be1b06fae9254b7523405aba9e97ed707fd1a412
tarun <167800861+tarun-kulkarni-coder@users.noreply.github.com>
2025-07-11 16:38:57 +0530

    Initial commit for Prescripto frontend

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
+
+        <div>
+          <img className='mb-5 w-40' src={assets.logo} alt="" />
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Prescripto is an online platform designed to simplify MediGuide appointment booking.Patients can connect with verified MediGuides, choose time slots, and manage visits seamlessly.Built for speed, security, and convenience – making healthcare accessible anytime, anywhere.</p>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About Us</Link></li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>+91 9632915734</li>
+            <li>tarunkulkarni4@gmail.com</li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2025 @ Prescripto.com - All Right Reserved.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
